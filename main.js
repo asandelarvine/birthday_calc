645356309e59d491a2961b9403604d23143a9856
asande <asandelarvine@gmail.com>
2021-09-06 00:17:49 +0300

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-function finder() {
-var date = document.getElementById("date").value;
-var month = document.getElementById("month").value;
-var year = document.getElementById("year").value;
-var y = new Array(date, month, year);
-//alert(y); 
+const akansForm = document.getElementById('akansForm');
+const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const calcBtn = document.getElementById('calculateButton');
+const result = document.getElementById('result');
 
-var date = new Date(y.reverse());
-date = date.getDay();
-alert(date);
-var month = [days of the month]
+function calculateDayOfBirth(birthDate, gender){
+
+    const dateOfBirth = new Date(birthDate);
+    const dayOfTheWeek = dateOfBirth.getDay();
+
+    const dayOfBirth = daysOfTheWeek[dayOfTheWeek];
+
+    return akanNames.find(function(akan){
+        return akan.dayOfBirth == dayOfBirth && akan.gender.toLowerCase() == gender.toLowerCase();
+    });
 
 }
+
+calcBtn.addEventListener('click', function(){
+    const akan = calculateDayOfBirth(akansForm.date.value, akansForm.gender.value );
+    result.innerHTML = akan.name;
+    // alert(akan.name);
+})
